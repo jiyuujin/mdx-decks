6178b810fd1bf174a87f32bd934cb466b359b0e6
jiyuujin <jiyuujinunite@gmail.com>
2021-11-09 14:16:17 +0900

    use preact instead

--- next.config.js
+++ next.config.js
@@ -3,5 +3,16 @@ const withMDX = require('@next/mdx')({
 })
 
 module.exports = withMDX({
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      }
+    }
+    return config
+  },
   pageExtensions: ['ts', 'tsx', 'mdx'],
 })
