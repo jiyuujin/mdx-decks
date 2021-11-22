0aebffdd44cd395ce75e77e9d4705b849969f0f5
jiyuujin <jiyuujinunite@gmail.com>
2021-11-22 13:23:34 +0900

    delete: AnimatePresence

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
-import { AnimatePresence } from 'framer-motion'
 import { CurrentSlideProvider } from '../context/CurrentSlideContext'
 import { ModeProvider } from '../context/ModeContext'
 import { MDXProvider } from '../components/MDXProvider'
@@ -20,15 +19,13 @@ const MyApp = ({ Component, pageProps }) => {
       <ThemeProvider theme={theme}>
         <CurrentSlideProvider>
           <ModeProvider>
-            <AnimatePresence exitBeforeEnter>
-              <Head>
-                <meta
-                  name="viewport"
-                  content="width=device-width, initial-scale=1, shrink-to-fit=no"
-                />
-              </Head>
-              <Component {...pageProps} />
-            </AnimatePresence>
+            <Head>
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1, shrink-to-fit=no"
+              />
+            </Head>
+            <Component {...pageProps} />
           </ModeProvider>
         </CurrentSlideProvider>
       </ThemeProvider>
